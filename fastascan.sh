de14538b7a0a9b8869d7a77d9bac1b5a23adb676
Waleria Popowska <waleria@Air-Valeria.home>
2023-11-21 13:14:52 +0100

    first commit

--- fastascan.sh
+++ fastascan.sh
@@ -1,2 +1,23 @@
 here is new script
 
+folder="."
+lines=0
+
+if  [ $# -ge 1 ]; then
+    folder="$1"
+fi
+
+if [ $# -ge 2 ]; then
+    lines="$2"
+fi
+
+echo "Searching for fasta/fa files in Folder: $folder"
+echo "Number of lines: $lines"
+
+find "$folder" -type f -name "*.fasta" -o -name "*.fa" | while read -r file; do
+    echo "Found: $file"
+     
+done
+
+
+
